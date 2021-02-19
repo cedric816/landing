8fa70c822904d0bd340d0b1d46eccd7f7865125e
cedric816 <73229376+cedric816@users.noreply.github.com>
2021-02-19 15:26:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include('data/data.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content= <?php echo $description_page ?>>
+    <meta name="description" content= '<?php echo $description_page ?>'>
     <title><?php echo $title_page ?></title>
     <!--style avec variables php-->
     <link rel='stylesheet' type='text/css' href='style.php' />
